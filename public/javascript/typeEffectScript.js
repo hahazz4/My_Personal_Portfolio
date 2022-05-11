8639f9a7765c7d6a2536dc54b7ffbaf9b0b7e52a
Zeerak Asim <Z_zee09@hotmail.com>
2022-05-11 13:52:19 -0400

    update 1.0

---
+++ public/javascript/typeEffectScript.js
@@ -0,0 +1,15 @@
+let i = 0;
+const message = "Hi my name is Zeerak Asim.";
+const speed = 50;
+
+function typeEffect() {
+    if (i < message.length){
+        document.getElementById("mainTStyle").innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeEffect, speed);
+    }
+}
+
+window.onload = function() {
+    typeEffect();
+  };
