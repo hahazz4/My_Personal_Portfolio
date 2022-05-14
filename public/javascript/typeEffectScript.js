630aefaa32fa3ec759b072f9b38749edf2288bed
Zeerak Asim <Z_zee09@hotmail.com>
2022-05-14 01:44:20 -0400

    update 1.5
    work in progress

--- public/javascript/typeEffectScript.js
+++ public/javascript/typeEffectScript.js
@@ -1,6 +1,11 @@
 let i = 0;
+let j = 0;
+let k = 0;
+
 const message = "Hi my name is Zeerak Asim.";
-const speed = 50;
+const message2 = "Welcome to my website! Here you can find information on who I am, my skills and my projects.";
+const message3 = "You can view my resume by clicking the button below, I wish you a good day!";
+const speed = 85;
 
 function typeEffect() {
     if (i < message.length){
@@ -8,8 +13,20 @@ function typeEffect() {
         i++;
         setTimeout(typeEffect, speed);
     }
+    
+    else if (j < message2.length){
+        document.getElementById("introPara1").innerHTML += message2.charAt(j);
+        j++;
+        setTimeout(typeEffect, speed);
+    }
+        
+    else if (k < message3.length){
+        document.getElementById("introPara2").innerHTML += message3.charAt(k);
+        k++;
+        setTimeout(typeEffect, speed);
+    }
 }
 
 window.onload = function() {
     typeEffect();
-  };
+};
