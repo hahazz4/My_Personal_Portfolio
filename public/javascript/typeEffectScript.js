fda6acc40a515211643ff61bfe36e2d0d3f4c55f
Zeerak Asim <Z_zee09@hotmail.com>
2022-05-15 04:21:49 -0400

    update 1.6
    changes lol

--- public/javascript/typeEffectScript.js
+++ public/javascript/typeEffectScript.js
@@ -3,9 +3,9 @@ let j = 0;
 let k = 0;
 
 const message = "Hi my name is Zeerak Asim.";
-const message2 = "Welcome to my website! Here you can find information on who I am, my skills and my projects.";
+const message2 = "Welcome to my website! Here you can find information about me, my skills and my projects.";
 const message3 = "You can view my resume by clicking the button below, I wish you a good day!";
-const speed = 85;
+const speed = 50;
 
 function typeEffect() {
     if (i < message.length){
